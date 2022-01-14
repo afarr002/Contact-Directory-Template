c206b217b006bc972e932aae4bdf839f735a12d5
aaron <afarrell002@gmail.com>
2022-01-14 14:22:57 -0700

    updated webpack.config to include htmlwebpack plugin webpackpwamanifest and generateSW and added output, plugins, and modules referencing them

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,27 +1,69 @@
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { GenerateSW } = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js',
-      cards: './src/js/cards.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
+      cards: "./src/js/cards.js",
     },
 
-    // TODO: Add the correct output
     output: {
-      
+      fielname: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
 
-    // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Contact Directory (template)",
+      }),
+
+      new GenerateSW(),
+      new WebpackPwaManifest({
+        name: "Contact Directory (template)",
+        short_name: "Contact Directory",
+        description: "Template for functionality of a Contact Directory",
+        background_color: "#FB4F14",
+        crossorigin: "use-credentials",
+        icons: [
+          {
+            src: path.resolve("./src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+        ],
+      }),
     ],
 
-    // TODO: Add the correct modules
     module: {
-
-    }
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
+      ],
+    },
   };
 };
