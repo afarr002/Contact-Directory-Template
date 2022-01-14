9bc8b8b28c7cf826e36fc811b42fe13afd6fea6b
aaron <afarrell002@gmail.com>
2022-01-14 15:56:45 -0700

    created logic for predetermined function expressions - initdb, postDb, and getDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,25 +1,58 @@
-// TODO: Install the following package:
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
-// TODO: Complete the initDb() function below:
-const initdb = async () => {
+const initdb = async () =>
+  openDB("contact", 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains("contact")) {
+        console.log("contact database already exists");
+        return;
+      }
+      db.createObjectStore("contact", {
+        keyPath: id,
+        autoIncrement: true,
+      });
+      console.log("contact database created!");
+    },
+  });
 
+export const postDb = async (name, homePhone, cellPhone, email) => {
+  console.log("POST to the database!");
+
+  const contactDB = await openDB("contact", 1);
+
+  const tx = contactDB.transaction("contact", "readwrite");
+
+  const store = tx.objectStore("contact");
+
+  const request = store.add({
+    name: name,
+    home_phone: homePhone,
+    cell_phone: cellPhone,
+    email: email,
+  });
+
+  const result = await request;
+
+  console.log("the data is off - saved to database", result);
 };
 
-
-// TODO: Complete the postDb() function below:
-export const postDb = async (name, home, cell, email)  => {
- 
-};
-
-// TODO: Complete the getDb() function below:
 export const getDb = async () => {
-  
+  console.log("GET from the database!");
+
+  const contactDB = await openDB("contact", 1);
+
+  const tx = contactDB.transaction("contact", "readonly");
+
+  const store = tx.objectStore("contact");
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log("result value", result);
+  return result;
 };
 
 // TODO: Complete the deleteDb() function below:
-export const deleteDb = async (id) => {
-  
-};
+export const deleteDb = async (id) => {};
 
 initdb();
