c7fffa6c3c2f517becf6d696e042cec97dd1c6fe
aaron <afarrell002@gmail.com>
2022-01-14 16:25:30 -0700

    created service workers to cache js and css files

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,1 +1,18 @@
-// TODO: Create a service worker that caches static assets:
+import { StaleWhileRevalidate } from "workbox-strategies";
+import { registerRoute } from "workbox-routing";
+import { CacheableResponsePlugin } from "workbox-cacheable-response";
+import { precacheAndRoute } from "workbox-precaching/precacheAndRoute";
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: "asset-cache",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
